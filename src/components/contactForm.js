d72d52bb4a615227d76780885d7a704af4135461
SaminYeaser <saminyeaser1@gmail.com>
2021-04-22 04:28:11 +0600

    form is building

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,8 +1,42 @@
-import react from 'react'
+import react, {useEffect, useState} from 'react'
 const ContactForm = () => {
+    const initialVlaues = {
+        fullName:'',
+        mobile:'',
+        email:'',
+        address:''
+    }
+    var [values,setValues] = useState(initialVlaues)
     return ( 
         <div>
-            Display Form
+            <form autoComplete="off" action="">
+                <div className='form-group input-group'>
+                    <div className='input-group-prepend'>
+                        <div className='input-group-text'>
+                            <i className='fas fa-user'></i>
+                        </div>
+                    </div>
+                    <input className='form-control' placeholder='Full Name' name='fullName' value={values.fullName} type="text"/>
+                </div>
+                <div className='form-row'>
+                <div className='form-group input-group col-md-6'>
+                    <div className='input-group-prepend'>
+                        <div className='input-group-text'>
+                            <i className='fas fa-mobile-alt'></i>
+                        </div>
+                    </div>
+                    <input className='form-control' placeholder='Mobile' name='mobile' value={values.mobile} type="text"/>
+                </div>
+                <div className='form-group input-group col-md-6'>
+                    <div className='input-group-prepend'>
+                        <div className='input-group-text'>
+                            <i className='fas fa-envelop'></i>
+                        </div>
+                    </div>
+                    <input className='form-control' placeholder='Email' name='email' value={values.email} type="text"/>
+                </div>
+                </div>
+            </form>
         </div>
      );
 }
